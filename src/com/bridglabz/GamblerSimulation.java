ba0082b780b009cd9feb91f0a0b073d3da0eaf75
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-04-25 21:57:50 +0530

    Added: Initialize Stake and Bet

--- src/com/bridglabz/GamblerSimulation.java
+++ src/com/bridglabz/GamblerSimulation.java
@@ -3,5 +3,7 @@ package com.bridglabz;
 public class GamblerSimulation {
     public static void main(String[] args) {
             System.out.println("Welcome To Gambling Simulation");
+        final int STAKE_PER_DAY = 100;
+        final int BET = 1;
         }
     }
